bc42c94c221b676b8f494aa0b10b33a0312a424e
Deborah Schirmer <dhschirmer@gmail.com>
2025-02-16 20:45:57 +0100

    Commit 2

--- src/MorseConverter/MorseConverter.java
+++ src/MorseConverter/MorseConverter.java
@@ -1,7 +1,42 @@
 package MorseConverter;
 
+import java.util.Scanner;
+
 public class MorseConverter {
     public static void main(String[] args) {
 
+
+        String text = "";
+        boolean iMeny = true;
+
+        while (iMeny) {
+
+            System.out.println("Välj ett av alternativen nedan:");
+            System.out.println("1 - Converter o codigo morse para ingles");
+            System.out.println("2 - Converter ingles para codigo morse");
+            System.out.println("3 - Sair");
+
+            Scanner scanner = new Scanner(System.in);
+            String menyVal = scanner.nextLine();
+
+
+            switch(menyVal){
+                case "1":
+                    System.out.println("1 - Converter o codigo morse para ingles");
+                    break;
+                case "2":
+                    System.out.println("2 - Converter ingles para codigo morse");
+                    break;
+                case "3":
+                    System.out.println("3 - Sair");
+                    iMeny = false;
+                    break;
+                default:
+                    System.out.println("Not a valid operation");
+
+            }
+        }
     }
 }
+
+
